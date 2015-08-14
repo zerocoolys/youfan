91018075220ee2da69da79134a828e667552528f
MrDeng <396187665@qq.com>
2015-08-14 14:10:49 +0800

    [add] 添加内容

--- youfan-platform/app.js
+++ youfan-platform/app.js
@@ -5,16 +5,19 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//自定义模块引用
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var partials = require('express-partials');
-
+var config = require("./config.json");
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine("html", require('ejs').renderFile);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,7 +26,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(partials());
+//业务需要引入模块
+///////////////////////////
+//自定义初始化内容
 
 app.use('/', routes);
 app.use('/users', users);
@@ -59,5 +64,6 @@ app.use(function(err, req, res, next) {
   });
 });
 
-
+app.get('/', routes);
+app.listen(3000);
 module.exports = app;
--- youfan-platform/routes/index.js
+++ youfan-platform/routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index.html', {});
 });
+//router.get("/user",function(req,res){
+//  res.send( 'Hello ,游客' );
+//});
 
+router.all("/login",function(req,res,next){
+  console.log("用户登录")
+  res.render("login.html", {});
+})
+router.all("/reg",function(req,res,next){
+  console.log("用户注册")
+  res.render("reg.html", {});
+})
+router.all("/user/:username",function(req,res,next){
+  console.log("检查用户名："+req.params.username)
+  next();
+})
+//一个路径绑定多个路由响应
+router.get("/user/:username",function(req,res,next){
+  //res.send( 'Hello ,'+req.params.username );
+  console.log("一个路径多个路由  Next响应")
+  next();
+});
+router.get("/user/:username",function(req,res){
+  res.send( req.params.username+" now is:"+new Date().toString() );
+});
 module.exports = router;
