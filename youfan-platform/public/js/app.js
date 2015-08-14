cca4f58adf57dd3924e4d576d728ad0d8e654e9a
MrDeng <396187665@qq.com>
2015-08-14 18:32:46 +0800

    [modify] 路由，登录页面 控制器

---
+++ youfan-platform/public/js/app.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Administrator on 2015/8/14.
+ */
+//х╗оч
+angular.module('app', [
+    'app.Authority',
+    //'app.SysManager',
+    //'app.ClientManager',
+    //'app.MerchantManager',
+])
+    //.config(['$stateProvider', '$urlRouterProvider',
+    //function (){
+    //    console.log("config")
+    //    //$stateProvider
+    //    //    .state('/', {
+    //    //        url: '/',
+    //    //        templateUrl: 'login.html'
+    //    //    });
+    //    //$stateProvider
+    //    //    .state('index', {
+    //    //        url: "/index",
+    //    //        templateUrl: "index.html"
+    //    //    })
+    //}])
---
+++ youfan-platform/public/js/controllers/Authority.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Administrator on 2015/8/14.
+ */
+
+//注册，系统管理 应该用不到
+angular.module('app.Authority', []).controller('regCtrl', function ($scope) {
+    console.log("regCtrl")
+});
+
+//修改信息
+angular.module('app.Authority', []).controller('editCtrl', function ($scope) {
+    console.log("regCtrl")
+});
+//登录
+angular.module('app.Authority', []).controller('loginCtrl', function ($scope,$state) {
+    console.log("loginCtrl")
+    $scope.count = "admin"
+
+    //登录
+    $scope.login = function () {
+        console.log("login")
+        $state.go("index.html");
+    }
+});
+
+//退出
+angular.module('app.Authority', []).controller('logoutCtrl', function ($scope) {
+    console.log("logoutCtrl")
+});
