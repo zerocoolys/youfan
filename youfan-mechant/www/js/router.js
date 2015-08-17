c8d62f521e52c32c6483dc933cd0ea2ac371e58d
TomDing729 <327523994@qq.com>
2015-08-17 17:27:14 +0800

    代码整合

---
+++ youfan-mechant/www/js/router.js
@@ -0,0 +1,32 @@
+(function() {
+    'use strict';
+
+
+    angular
+        .module('yf_mechant')
+        .config(function ($stateProvider, $urlRouterProvider) {
+
+
+
+            // login
+            $stateProvider
+                .state('login', {
+                    url: '/login',
+                    templateUrl: 'templates/login/login.html'
+
+                });
+
+
+            $stateProvider
+                .state('overview', {
+                    url: '/overview',
+                    templateUrl: 'templates/overview/overview.html',
+                    controller: 'overview'
+
+                });
+
+            $urlRouterProvider.otherwise('/login');
+
+
+        });
+})();
