dc7368e88594ddffb5d4ba28187bef430446a2a9
weims <449598723@qq.com>
2015-08-17 18:41:53 +0800

    [youfan-merchant]设置-帮助

--- youfan-mechant/www/js/controller/overview_controller.js
+++ youfan-mechant/www/js/controller/overview_controller.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('yf_mechant')
+        .module('yf_merchant')
         .controller('overview', Overview);
 
     function Overview($scope, $filter, $state) {
--- youfan-mechant/www/js/controller/settings-controller.js
+++ youfan-mechant/www/js/controller/settings-controller.js
@@ -1,4 +1,4 @@
-angular.module('starter.settings_controllers', [])
+angular.module('yf_merchant.settings_controllers', ['yf_merchant.settings_service'])
 
     .controller('SettingsIndexCtrl', function ($scope) {
         $scope.items = [{
@@ -9,20 +9,72 @@ angular.module('starter.settings_controllers', [])
             "title": "帮助",
             "description": "帮助", "img": "https://avatars3.githubusercontent.com/u/11214?v=3&s=460"
         }, {"title": "免责声明", "description": "免责声明", "img": "https://avatars3.githubusercontent.com/u/11214?v=3&s=460"}];
-        console.log("22222");
 
     })
 
-    .controller('SettingsHelpCtrl', function ($scope) {
-
-        console.log("1111111111");
-
+    .controller('SettingsHelpCtrl', function ($scope, HelpService) {
+        $scope.help_items = HelpService.all();
     })
 
+    .controller('SettingsHelpDetailCtrl', function ($scope, $stateParams, HelpService) {
+
+        $scope.obj = HelpService.get($stateParams.helpId);
+
+        console.log($scope.obj);
+    })
     .controller('SettingsDisclaimerCtrl', function ($scope) {
-
-        console.log("333333");
-
+        $scope.disclaimerText = "";
+        $scope.doRefresh = function () {
+            $scope.disclaimerText = "时间煮雨";
+            $scope.$broadcast("scroll.refreshComplete");
+        };
     })
 
+    .config(function ($stateProvider, $urlRouterProvider) {
+
+        // 设置
+        $stateProvider
+            .state('settings', {
+                url: '/settings',
+                abstract: true,
+                templateUrl: 'templates/settings/settings.html'
+            })
+            .state('settings.index', {
+                url: '/index',
+                views: {
+                    'weims': {
+                        templateUrl: 'templates/settings/index.html',
+                        controller: 'SettingsIndexCtrl'
+                    }
+                }
+            })
+            .state('settings.helps', {
+                url: '/helps',
+                views: {
+                    'weims': {
+                        templateUrl: 'templates/settings/help/help.html',
+                        controller: 'SettingsHelpCtrl'
+                    }
+                }
+            })
+            .state('settings.help-detail', {
+                url: '/helps/:helpId',
+                views: {
+                    'weims': {
+                        templateUrl: 'templates/settings/help/help-detail.html',
+                        controller: 'SettingsHelpDetailCtrl'
+                    }
+                }
+            })
+            .state('settings.disclaimer', {
+                url: '/disclaimer',
+                views: {
+                    'weims': {
+                        templateUrl: 'templates/settings/disclaimer.html',
+                        controller: 'SettingsDisclaimerCtrl'
+                    }
+                }
+            });
+
+    })
 ;
