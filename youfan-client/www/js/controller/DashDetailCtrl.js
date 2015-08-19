9420f5c0562e09c65b41eedb23bec9db2a9ce670
dolphineor <dolphineor@gmail.com>
2015-08-19 17:03:03 +0800

    [menu] 菜单选购

--- youfan-client/www/js/controller/DashDetailCtrl.js
+++ youfan-client/www/js/controller/DashDetailCtrl.js
@@ -14,22 +14,39 @@ ControllerModule.controller('DashDetailCtrl', function ($scope, $http, $ionicSli
         $ionicSlideBoxDelegate.slide(index);
     };
 
+    $scope.sellerId = 196830201;
+
     $scope.menuArr = [];
 
-    $http.get(REST_URL + '/menu/list/196830201').success(function (data) {
-        $scope.menuArr = JSON.parse(data.menus);
+    $http.get(REST_URL + '/menu/list/' + $scope.sellerId).success(function (data) {
+        var jsonArr = JSON.parse(data.menus);
+
+        for (var i = 0, l = jsonArr.length; i < l; i++) {
+            jsonArr[i].price = parseFloat(jsonArr[i].price).toFixed(2);
+        }
+
+        $scope.menuArr = jsonArr;
     });
 
     $scope.cart = new Map();
 
-    $scope.addToCart = function (menuId, restNum) {
-        if (restNum > 0) {
-            $scope.cart.put(menuId, menuId)
+    $scope.addToCart = function (menu) {
+        if (menu.restNum > 0) {
+            $scope.cart.put(menu.menuId, menu);
+
+            var total = 0;
+            $scope.cart.values().forEach(function (item, i) {
+                total += item.price;
+            });
+
+            $scope.subtotal = parseFloat(total).toFixed(2);
         }
     };
 
     $scope.removeFromCart = function (menuId) {
         $scope.cart.remove(menuId)
-    }
+    };
+
+    $scope.subtotal = 0;
 
 });
