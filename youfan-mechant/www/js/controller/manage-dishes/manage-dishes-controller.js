4bebd29e0a73ad8985cb50c3f56e54e6fae1b74d
weims <449598723@qq.com>
2015-08-20 17:26:59 +0800

    [youfan-merchant]管理菜品

---
+++ youfan-mechant/www/js/controller/manage-dishes/manage-dishes-controller.js
@@ -0,0 +1,211 @@
+angular.module('yf_merchant.manage_dishes_controllers', ['yf_merchant.manage-dishes_service'])
+
+    .controller('ManageDishesNscCtrl', function ($scope, $state, $ionicLoading, $timeout) {
+
+        console.log("ManageDishesNscCtrl");
+
+        $scope.items = [];
+
+        $scope.addNsc = function () {
+            $state.go("m_dishes_nsc_add");
+        };
+
+        $scope.load = function () {
+            $ionicLoading.show({
+                template: "正在载入数据，请稍后..."
+            });
+            //延时2000ms来模拟载入的耗时行为
+            var idx = 0;
+            var max = Math.ceil(Math.random() * 7);
+            $timeout(function () {
+                for (var i = 0; i < max; i++, idx++) $scope.items.unshift({
+                    name: "鱼香肉丝",
+                    price: Math.ceil(Math.random() * 100),
+                    url: "https://avatars3.githubusercontent.com/u/11214?v=3&s=460"
+                });
+                //隐藏载入指示器
+                $ionicLoading.hide();
+            }, 2000);
+            $scope.isActive = false;
+        };
+
+        $scope.load();
+
+    })
+
+    .controller('ManageDishesXfzCtrl', function ($scope) {
+
+        console.log("ManageDishesXfzCtrl");
+
+
+    })
+
+    .controller('ManageDishesQtcCtrl', function ($scope, $state) {
+
+        console.log("ManageDishesQtcCtrl");
+
+        $scope.items = [];
+
+        $scope.addQtc = function () {
+            $state.go("m_dishes_qtc_add");
+        };
+
+
+    })
+
+    .controller('ManageDishesNscAddCtrl', function ($scope, $state, $ionicActionSheet, $ionicLoading, $timeout, KwService, ManageDishesService) {
+
+        console.log("ManageDishesNscAddCtrl");
+
+        $scope.kwItems = KwService.all();
+
+        $scope.dishes = {staple: false};
+        $scope.imgs = [];
+        $scope.isActive = false;
+
+        $scope.addNscPic = function () {
+            console.log("addNscPic");
+            // Show the action sheet
+            var hideSheet = $ionicActionSheet.show({
+                buttons: [
+                    {text: "打开相机"},
+                    {text: "打开相册"}
+                ],
+                buttonClicked: function (index) {
+                    if (!navigator.camera) {
+                        alert('请在真机环境中使用相册功能。现在只是模拟一张图片')
+                    }
+                    $scope.imgs.push({
+                        index: $scope.imgs.length,
+                        url: "https://avatars3.githubusercontent.com/u/11214?v=3&s=460"
+                    });
+                    return true;
+                },
+                cancelText: "取消",
+                cancel: function () {
+                    // add cancel code..
+                }
+            });
+
+            // For example's sake, hide the sheet after two seconds
+            $timeout(function () {
+                //	hideSheet();
+            }, 2000);
+
+        };
+
+        $scope.doCheckDishes = function () {
+            $scope.isActive = true;
+            if ($scope.imgs.length == 0) {
+                alert("请添加菜品图片");
+                $scope.isActive = false;
+            } else {
+                $scope.doSave();
+            }
+        };
+
+        $scope.doSave = function () {
+            $ionicLoading.show({
+                template: "保存菜品中，请稍后..."
+            });
+            $timeout(function () {
+                ManageDishesService.saveDishes($scope.dishes);
+            }, 1000);
+
+
+        };
+
+        $scope.$on("yf-merchant-save-dishes-success", function () {
+            $state.go("m_dishes.nsc");
+        });
+
+        $scope.$on("yf-merchant-save-dishes-error", function () {
+            alert("系统错误");
+            $ionicLoading.hide();
+            $scope.isActive = false;
+        });
+
+    })
+
+    .controller('ManageDishesQtcAddCtrl', function ($scope, $ionicActionSheet, $timeout, ManageDishesService) {
+
+        console.log("ManageDishesQtcAddCtrl");
+
+        $scope.addQtcPic = function () {
+            console.log("addNscPic");
+            // Show the action sheet
+            var hideSheet = $ionicActionSheet.show({
+                buttons: [
+                    {text: "打开相机"},
+                    {text: "打开相册"}
+                ],
+                buttonClicked: function (index) {
+                    console.log(index);
+                    return true;
+                },
+                cancelText: "取消",
+                cancel: function () {
+                    // add cancel code..
+                }
+            });
+
+            // For example's sake, hide the sheet after two seconds
+            $timeout(function () {
+                //	hideSheet();
+            }, 2000);
+
+        };
+
+
+    })
+
+    .config(function ($stateProvider, $urlRouterProvider) {
+
+        // 设置
+        $stateProvider
+            .state('m_dishes', {
+                url: '/manage/dishes',
+                abstract: true,
+                templateUrl: 'templates/manage-dishes/manage-dishes-entrance.html'
+            })
+            .state('m_dishes.nsc', {
+                url: '/nsc',
+                views: {
+                    'm_dishes_nsc': {
+                        controller: 'ManageDishesNscCtrl',
+                        templateUrl: 'templates/manage-dishes/manage-dishes-nsc.html'
+                    }
+                }
+            })
+            .state('m_dishes.xfz', {
+                url: '/xfz',
+                views: {
+                    'm_dishes_xfz': {
+                        controller: 'ManageDishesXfzCtrl',
+                        templateUrl: 'templates/manage-dishes/manage-dishes-xfz.html'
+                    }
+                }
+            })
+            .state('m_dishes.qtc', {
+                url: '/qtc',
+                views: {
+                    'm_dishes_qtc': {
+                        controller: 'ManageDishesQtcCtrl',
+                        templateUrl: 'templates/manage-dishes/manage-dishes-qtc.html'
+                    }
+                }
+            })
+            .state('m_dishes_nsc_add', {
+                url: '/manage/dishes/nsc/add',
+                controller: 'ManageDishesNscAddCtrl',
+                templateUrl: 'templates/manage-dishes/manage-dishes-nsc-add.html'
+            })
+            .state('m_dishes_qtc_add', {
+                url: '/manage/dishes/qtc/add',
+                controller: 'ManageDishesQtcAddCtrl',
+                templateUrl: 'templates/manage-dishes/manage-dishes-qtc-add.html'
+            })
+        ;
+
+    })
+;
