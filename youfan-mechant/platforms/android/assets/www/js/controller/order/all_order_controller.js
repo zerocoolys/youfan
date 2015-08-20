cb49531ac3396005bedf13f630e55dce3f4b1f2c
liwenkaifzk <878679630@qq.com>
2015-08-20 10:06:24 +0800

    【youfan-merchant】日历更新

--- youfan-mechant/platforms/android/assets/www/js/controller/order/all_order_controller.js
+++ youfan-mechant/platforms/android/assets/www/js/controller/order/all_order_controller.js
@@ -5,8 +5,23 @@
         .module('yf_merchant')
         .controller('all_order', AllOrder);
 
-    function AllOrder($scope, $filter, $state) {
+    function AllOrder($scope, $filter, $state,$ionicSlideBoxDelegate) {
 
 
+        $scope.scopeIndex = 0;
+
+        $scope.statusIndex = 0;
+
+
+        $scope.activeScope = function (index){
+             $scope.scopeIndex=index;
+            $ionicSlideBoxDelegate.slide(index);
+
+        };
+
+        $scope.activeStats = function (index) {
+            $scope.statusIndex = index;
+            $ionicSlideBoxDelegate.slide(index);
+        };
     }
 })();
