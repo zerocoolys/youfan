8f12217dcf0db6dae1ad2eaa33a8188386b3ad62
TomDing729 <327523994@qq.com>
2015-08-21 09:49:56 +0800

    开发-饭友评价界面

--- youfan-mechant/www/js/controller/comment/customer_comment_controller.js
+++ youfan-mechant/www/js/controller/comment/customer_comment_controller.js
@@ -5,14 +5,56 @@
         .module('yf_merchant')
         .controller('customer_comment', CustomerComment);
 
-    function CustomerComment($scope, $filter, $state) {
+    function CustomerComment($scope, $filter, $state,$ionicPopup) {
+
 
 
         $scope.statusIndex = 0;
+        $scope.data = {};
 
         $scope.activeStats = function (index) {
             $scope.statusIndex = index;
             $ionicSlideBoxDelegate.slide(index);
         };
+
+
+        $scope.comments = [
+            {img: "img/2.jpeg", userName: 'Tom', content: '好好吃1', ctime: "2015-02-03 14:59:03",mtime:"2015-02-03 16:59:03",replyComments:""},
+            {img: "img/2.jpeg", userName: '王小明', content: '好好吃2', ctime: "2015-02-03 14:59:03",mtime:"2015-02-03 16:59:03",replyComments:"谢谢"},
+            {img: "img/2.jpeg", userName: '莎莎', content: '好好吃3', ctime: "2015-02-03 14:59:03",mtime:"2015-02-03 16:59:03",replyComments:"谢谢"},
+            {img: "img/2.jpeg", userName: '小李', content: '好好吃4', ctime: "2015-02-03 14:59:03",mtime:"2015-02-03 16:59:03",replyComments:"谢谢"},
+            {img: "img/2.jpeg", userName: '欧阳赤兔', content: '好好吃5', ctime: "2015-02-03 14:59:03",mtime:"2015-02-03 16:59:03",replyComments:"谢谢"}
+        ]
+
+        $scope.replyComments = function() {
+            $ionicPopup.show({
+                template: '<textarea ng-model="data.comment"> </textarea>',
+                title: '评论回复',
+                scope: $scope,
+                buttons: [
+                    { text: 'Cancel' },
+                    {
+                        text: '<b>Save</b>',
+                        type: 'button-positive',
+                        onTap: function(e) {
+                            console.log($scope.comments);
+                            $scope.comments[0].replyComments  = $scope.data.comment;
+
+                            console.log($scope.comments);
+
+
+                            if (!$scope.data.comment) {
+                                e.preventDefault();
+                            } else {
+                                return $scope.data.comment;
+                            }
+                        }
+                    },
+                ]
+            });
+
+
+        }
+
     }
 })();
