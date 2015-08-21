eb898163ee564237ee26ca15f9ea452acf87b1b1
dengchanglu <dengchanglu_ss@163.com>
2015-08-21 17:14:14 +0800

    [youfan-merchant]因mongobaseDao的修改而整改

--- youfan-server/src/main/java/com/youfan/data/dao/UserDAO.java
+++ youfan-server/src/main/java/com/youfan/data/dao/UserDAO.java
@@ -1,9 +1,10 @@
 package com.youfan.data.dao;
 
+import com.youfan.controllers.objs.MerchantUser;
 import com.youfan.data.models.UserEntity;
 
 /**
  * Created by perfection on 15-8-19.
  */
-public interface UserDAO extends MongoBaseDAO<UserEntity, Long> {
+public interface UserDAO extends MongoBaseDAO<UserEntity,MerchantUser, Long> {
 }
