34a3005a5d8410290786fddf70a867f4bcadbe4d
zymin <737164008@qq.com>
2015-08-25 18:14:01 +0800

    [youfan-client]优化订单、个人信息页面

--- youfan-client/www/js/controller/MyOrderCtrl.js
+++ youfan-client/www/js/controller/MyOrderCtrl.js
@@ -10,4 +10,35 @@ ControllerModule.controller('MyOrderCtrl', function ($scope, $stateParams, $ioni
     $scope.activeSlide = function (index) {
         $ionicSlideBoxDelegate.slide(index);
     };
+
+//    订单详情
+    $scope.orderUnfinishes = [{
+        number:'135268549',
+        status:'待付款',
+        cookName:'河马堂药膳厨房',
+        singleTime:'2014.11.08 下午13:00',
+        price:'￥98.00',
+        payWorld:'立即支付'
+    },{
+        number:'135268549',
+        status:'待付款',
+        cookName:'河马堂药膳厨房',
+        singleTime:'2014.11.08 下午13:00',
+        price:'￥98.00',
+        payWorld:'立即支付'
+    },{
+        number:'135268549',
+        status:'待付款',
+        cookName:'河马堂药膳厨房',
+        singleTime:'2014.11.08 下午13:00',
+        price:'￥98.00',
+        payWorld:'立即支付'
+    },{
+        number:'135268549',
+        status:'待付款',
+        cookName:'河马堂药膳厨房',
+        singleTime:'2014.11.08 下午13:00',
+        price:'￥98.00',
+        payWorld:'立即支付'
+    }]
 });
