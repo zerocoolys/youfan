fc3248d12b4ddde5264086de79d7a36f862af979
TomDing729 <327523994@qq.com>
2015-08-25 19:36:07 +0800

    订单界面调整

--- youfan-mechant/www/js/controller/dishes_controller.js
+++ youfan-mechant/www/js/controller/dishes_controller.js
@@ -8,24 +8,24 @@
         .module('yf_merchant')
         .controller('dishes', dishes);
 
-    function dishes($scope, $filter, $state, $ionicSlideBoxDelegate,$stateParams) {
+    function dishes($scope, $filter, $state, $ionicSlideBoxDelegate,$stateParams,$http,$rootScope) {
 
-        $scope.ways = [{name: "配送", id: 0}, {name: "上门", id: 1}, {name: "要做的菜", id: 2}];
+        $scope.ways = [{name: "配送", id: 'PS'}, {name: "上门", id: 'SM'}, {name: "要做的菜", id: 'YZDC'}];
 
-        $scope.dishesIndex = 0;
+
+        $scope.headerIndex = 'PS';
         if($stateParams.path == 'dishes') {
-            $scope.dishesIndex = 2;
+            $scope.headerIndex = 'YZDC';
         }
 
-        $scope.activedishes = function (dishesIndex) {
-            $scope.dishesIndex = dishesIndex;
+        $scope.selectedHeader = function (headerIndex) {
+            $scope.headerIndex = headerIndex;
             $ionicSlideBoxDelegate.enableSlide([false])
-
         };
 
-        $scope.slideIndex = 1;
-        $scope.activeSlide = function (slideIndex) {
-            $scope.slideIndex = slideIndex;
+        $scope.statusIndex = 1;
+        $scope.activeSlide = function (statusIndex) {
+            $scope.statusIndex = statusIndex;
             $ionicSlideBoxDelegate.enableSlide([false]);
         };
 
@@ -36,6 +36,57 @@
             {name: "退款中", id: 31, number: 0},
             {name: "已退款", id: 39, number: 0}];
 
+
+        $scope.disposeOrderOrDishData = function (datas) {
+            if(datas == null) {
+                return;
+            }
+
+
+
+
+        }
+
+
+
+        $scope.loadOrderOrDishData = function () {
+            var url = "";
+            var merchant = {};
+            //订单或菜品
+            if($scope.dishesIndex == 2) {
+                url = "";
+            } else {
+                url = "http://127.0.0.1:8080/orders/merchants/";
+                //订单（配送或上门）
+                if($scope.dishesIndex == 0) {
+                    merchant.repastMode = "PS";
+                } else if($scope.dishesIndex == 1){
+                    merchant.repastMode = "SM";
+                }
+            }
+            merchant.sellerId = "2";
+            merchant.orderStatus = $scope.statusIndex;
+
+            url = url+ JSON.stringify(merchant);
+
+            $http.get(url).success
+            (function (res) {
+                    if(res == null) {
+                        alert("后台正在更新，请稍等。");
+                        return;
+                    }
+                    if(res.code == 1) {
+                        alert(res.msg);
+                        return;
+                    }
+
+                $scope.disposeOrderOrDishData(res.payload);
+            });
+        }
+
+
+
+        $scope.loadOrderData();
 
         $scope.orders = [
             {
