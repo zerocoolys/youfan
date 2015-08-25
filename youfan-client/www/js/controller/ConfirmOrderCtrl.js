8d90a57c0dedabd74bfa8ce20adaea836f823973
dolphineor <dolphineor@gmail.com>
2015-08-25 18:16:04 +0800

    [order] 订单页面初步处理

--- youfan-client/www/js/controller/ConfirmOrderCtrl.js
+++ youfan-client/www/js/controller/ConfirmOrderCtrl.js
@@ -16,15 +16,47 @@ ControllerModule.controller('ConfirmOrderCtrl', function ($scope, $ionicModal, O
     });
 
     // 订单
-    $scope.order = {};
+    $scope.order = {
+        items: [],  // 菜品
+        price: 0,
+        repastMode: '',  // 就餐方式
+        repastAddress: '',  // 就餐地址
+        coupons: 0,  // 优惠券
+        comments: ''    // 备注
+    };
+
+    // 购物车
+    $scope.cart = {};
+
+    // 此次订单的总价
+    $scope.totalPrice = 0;
+
+    // 剩余支付
+    $scope.remainPayedPrice = 0;
 
     // 支付
     $scope.pay = function () {
-        console.log("Pay ...");
+        $http.post(REST_URL + '/orders/create', $scope.order).
+            then(function (response) {
+                console.log("Success");
+            }, function (err) {
+                console.log(err);
+            });
     };
 
     $scope.loadCart = function () {
-        console.log(JSON.stringify(Order.details.cart));
+        $scope.cart.data = Order.details.cart;
+
+        var _total = 0;
+        $scope.cart.data.forEach(function (item) {
+            _total += parseFloat(item.totalPrice);
+        });
+
+        $scope.totalPrice = _total.toFixed(2);
+
+        // TODO 计算剩余支付, 当前默认使用订单总价
+        $scope.remainPayedPrice = $scope.totalPrice;
+
     }
 
 });
