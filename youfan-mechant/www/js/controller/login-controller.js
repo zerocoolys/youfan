d78af8664b95a087e79d1fc1274ac19b23c66e33
dengchanglu <dengchanglu_ss@163.com>
2015-08-27 16:49:39 +0800

    [youfan-merchant]登陆改为手机登陆

--- youfan-mechant/www/js/controller/login-controller.js
+++ youfan-mechant/www/js/controller/login-controller.js
@@ -8,40 +8,102 @@
         .module('yf_merchant')
         .controller('merchant_login', merchant_login);
     function merchant_login($scope, $filter, $state, $rootScope, $http, $location, $ionicPopup) {
-        $scope.signIn = function (user) {
-            $http.post(
-                "http://192.168.1.110:8080/user/login", JSON.stringify(user), {"Content-Type": "application/json;charset=utf-8"}).success(function (data) {
+        $scope.user={
+            phoneNumber:"",
+            verificationCode:""
+        };
+        $scope.getVerificationCode = function(user){
+            $scope.verificationCode = "";
+            //生成 6 位随机数验证码
+            for ( var i = 0; i < 6; i++) {
+                $scope.verificationCode += parseInt(Math.random() * 9).toString();
+            }
+            // 向后台发送处理数据
+            $scope.url = "http://192.168.1.107:8080/platform/sendSMS/1/" + $scope.verificationCode + "/" + user.phoneNumber;
+            $http.get($scope.url).success(function(data) {
+                var options;
+                if(data.statusCode=="000000"){
+                    options = {
+                        "title": "验证码发送成功!",
+                        "buttons": [{
+                            text: "确定",
+                            type: "button-positive clam",
+                            onTap: function() {
+                                $scope.user={
+                                    phoneNumber:user.phoneNumber,
+                                    verificationCode:$scope.verificationCode
+                                };
+                            }
+                        }]
+                    };
+                }else{
+                    options = {
+                        "title": "验证码发送失败!",
+                        "buttons": [{
+                            text: "关闭",
+                            type: "button-positive clam"
+                        }]
+                    };
+                }
+                $ionicPopup.alert(options)
+                    .then(function () {
 
-                    if (data == "" || data == null) {
-                        var options = {
-                            "title": "瀵嗙爜鎴栬处鍙烽敊璇紒",
+                    });
+            });
+
+        };
+        $scope.signIn = function (user) {
+            if(user.verificationCode==$scope.verificationCode){
+                var merchantUser = {
+                    userName:user.phoneNumber
+                };
+                $http.post(
+                    "http://192.168.1.110:8080/user/login", JSON.stringify(merchantUser), {"Content-Type": "application/json;charset=utf-8"}).success(function (data) {
+
+                        if (data == "" || data == null) {
+                            var options = {
+                                "title": "系统繁忙！",
+                                "buttons": [{
+                                    text: "关闭",
+                                    type: "button-positive clam"
+                                }]
+                            };
+                            $ionicPopup.alert(options)
+                                .then(function () {
+                                    //这个函数在弹出框关闭时被调用
+                                });
+                        } else {
+                            $scope.user = data;
+                            $rootScope.user = $scope.user;
+                            $location.path("overview")
+                        }
+
+                    }, function (error) {
+                        options = {
+                            "title": "系统繁忙！",
                             "buttons": [{
-                                text: "鍏抽棴",
+                                text: "关闭",
                                 type: "button-positive clam"
                             }]
                         };
                         $ionicPopup.alert(options)
                             .then(function () {
-                                //杩欎釜鍑芥暟鍦ㄥ脊鍑烘鍏抽棴鏃惰璋冪敤
                             });
-                    } else {
-                        $scope.user = data;
-                        $rootScope.user = $scope.user;
-                        $location.path("overview")
-                    }
+                    });
 
-                }, function (error) {
-                    options = {
-                        "title": "绯荤粺绻佸繖锛�",
-                        "buttons": [{
-                            text: "鍏抽棴",
-                            type: "button-positive clam"
-                        }]
-                    };
-                    $ionicPopup.alert(options)
-                        .then(function () {
-                        });
-                });
+            }else{
+                var options = {
+                    "title": "验证码输入不正确！",
+                    "buttons": [{
+                        text: "关闭",
+                        type: "button-positive clam"
+                    }]
+                };
+                $ionicPopup.alert(options)
+                    .then(function () {
+                        //这个函数在弹出框关闭时被调用
+                    });
+            }
 
         };
         $scope.register = function (user) {
@@ -49,13 +111,13 @@
                 "http://192.168.1.110:8080/user/register", JSON.stringify(user), {"Content-Type": "application/json;charset=utf-8"}).success(function (data) {
 
                     var options;
-                    switch (data.registerStatus){
+                    switch (data.registerStatus) {
 
                         case "1":
                             options = {
-                                "title": "娉ㄥ唽鎴愬姛锛�",
+                                "title": "注册成功！",
                                 "buttons": [{
-                                    text: "纭畾",
+                                    text: "确定",
                                     type: "button-positive clam"
                                 }]
                             };
@@ -68,9 +130,9 @@
                             break;
                         case "0":
                             options = {
-                                "title": "娉ㄥ唽澶辫触锛�",
+                                "title": "注册失败！",
                                 "buttons": [{
-                                    text: "鍏抽棴",
+                                    text: "关闭",
                                     type: "button-positive clam"
                                 }]
                             };
@@ -80,9 +142,9 @@
                             break;
                         case "-1":
                             options = {
-                                "title": "绯荤粺绻佸繖锛�",
+                                "title": "系统繁忙！",
                                 "buttons": [{
-                                    text: "鍏抽棴",
+                                    text: "关闭",
                                     type: "button-positive clam"
                                 }]
                             };
@@ -93,9 +155,9 @@
                     }
                 }, function (error) {
                     options = {
-                        "title": "绯荤粺绻佸繖锛�",
+                        "title": "系统繁忙！",
                         "buttons": [{
-                            text: "鍏抽棴",
+                            text: "关闭",
                             type: "button-positive clam"
                         }]
                     };
