88106855f4d17d2da9413c23a4917d2a295b1d24
zymin <737164008@qq.com>
2015-08-31 18:36:12 +0800

    [youfan-client]我的订单框架修改

--- youfan-client/www/js/controller/MyOrderCtrl.js
+++ youfan-client/www/js/controller/MyOrderCtrl.js
@@ -10,8 +10,8 @@ ControllerModule.controller('MyOrderCtrl', function ($scope, $stateParams, $ioni
     $scope.activeSlide = function (index) {
         $ionicSlideBoxDelegate.slide(index);
     };
-
 //    订单详情
+    /*未完成的订单*/
     $scope.orderUnfinishes = [{
         number:'135268549',
         status:'待评价',
@@ -40,5 +40,53 @@ ControllerModule.controller('MyOrderCtrl', function ($scope, $stateParams, $ioni
         singleTime:'2014.11.08 下午13:00',
         price:'￥98.00',
         payWorld:'立即支付'
+    },{
+        number:'135268549',
+        status:'待付款',
+        cookName:'河马堂药膳厨房',
+        singleTime:'2014.11.08 下午13:00',
+        price:'￥98.00',
+        payWorld:'立即支付'
+    },{
+        number:'135268549',
+        status:'待付款',
+        cookName:'河马堂药膳厨房',
+        singleTime:'2014.11.08 下午13:00',
+        price:'￥98.00',
+        payWorld:'立即支付'
+    },{
+        number:'135268549',
+        status:'待付款',
+        cookName:'河马堂药膳厨房',
+        singleTime:'2014.11.08 下午13:00',
+        price:'￥98.00',
+        payWorld:'立即支付'
+    },{
+        number:'135268549',
+        status:'待付款',
+        cookName:'河马堂药膳厨房',
+        singleTime:'2014.11.08 下午13:00',
+        price:'￥98.00',
+        payWorld:'立即支付'
     }]
+    /*已完成的订单*/
+    $scope.orderFinishes = [{
+        number:'135268549',
+        status:'待付款',
+        cookName:'河马堂药膳厨房',
+        singleTime:'2014.11.08 下午13:00',
+        price:'￥98.00'
+    },{
+        number:'135268549',
+        status:'待付款',
+        cookName:'河马堂药膳厨房',
+        singleTime:'2014.11.08 下午13:00',
+        price:'￥98.00'
+    },{
+        number:'135268549',
+        status:'待付款',
+        cookName:'河马堂药膳厨房',
+        singleTime:'2014.11.08 下午13:00',
+        price:'￥98.00'
+    }];
 });
