0bd6b1ea27c7d23845819dd5625c82cea225c76c
XiaoWei <xiaoweiqb@126.com>
2015-09-01 11:29:43 +0800

    [client-merchant] RESET_URL 为 localhost:8080

--- youfan-client/www/js/config.js
+++ youfan-client/www/js/config.js
@@ -1,3 +1,3 @@
 var ConfigModule = angular.module('ConfigModule', [])
-    .constant('REST_URL', 'http://192.168.1.100:8080')
+    .constant('REST_URL', 'http://localhost:8080')
     .constant("HTTP_HEAD","http://");
