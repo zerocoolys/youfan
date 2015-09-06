39f98e66b0c4369d50f7e769c2307962f56ec877
dengchanglu <dengchanglu_ss@163.com>
2015-09-06 18:43:44 +0800

    [youfan-merchant]完成编辑厨房所有的信息的保存和查询，部分信息的验证为完成

--- youfan-merchant/www/js/controller/util/cameraUtil.js
+++ youfan-merchant/www/js/controller/util/cameraUtil.js
@@ -1,15 +1,15 @@
 /**
  * Created by perfection on 15-9-1.
  */
-var imageUrlPrefix = "http://kitchenimage.b0.upaiyun.com/";
-var upYunForm_API_key = "CNGPrBl5TAbJ/FpfYk1UjdRlQhg=";
-var upYunSpaceName = "kitchenimage";
-var upYunSavePath = "/kitchenimage/{random}{.suffix}";
+var imageUrlPrefix = "http://youfan-merchant-img.b0.upaiyun.com/";
+var upYunForm_API_key = "+GJEDJwQ6qecG6Er+VrFEPektEo=";
+var upYunSpaceName = "youfan-merchant-img";
+var upYunSavePath = "{random}{.suffix}";
 var createActionSheet = function (buttonId,$ionicActionSheet, $scope, $cordovaCamera) {
     $ionicActionSheet.show({
         buttons: [
             {text: '<p class="calm text-center"  >拍照</p>'},
-            {text: '<p class="calm text-center" >从相册中选取</p>'},
+            {text: '<p class="calm text-center" >从相册中选取</p>'}
         ],
         cancelText: '<p  class="calm">取消</p>',
         buttonClicked: function (index) {
@@ -96,14 +96,13 @@ var uploadImg = function (buttonId, imageUrl,$ionicLoading,$scope) {
 
 
     var ft = new FileTransfer();
-    alert(ft);
     ft.upload(fileURL, "http://v0.api.upyun.com/"+upYunSpaceName, function (data) {
-        alert(data);
         var result = JSON.parse(data.response);
         if (result.code == 200) {
-            var htmlImageUrl = imageUrlPrefix+result.url;
-            $scope.saveImagePath(buttonId, htmlImageUrl);
             $ionicLoading.hide();
+            var htmlImageUrl = imageUrlPrefix+result.url;
+            $scope.saveImagePath(buttonId, htmlImageUrl.split("?")[0]);
+
         } else {
             $ionicLoading.hide();
             alert("上传失败");
