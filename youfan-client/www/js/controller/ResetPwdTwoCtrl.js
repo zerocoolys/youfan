011498549778462f28fc5284ac37ceb06cdae625
zymin <737164008@qq.com>
2015-09-06 11:17:00 +0800

    [youfan-client]密码重置页面分布进行

---
+++ youfan-client/www/js/controller/ResetPwdTwoCtrl.js
@@ -0,0 +1,6 @@
+/**
+ * Created by ss on 2015/9/6.
+ */
+ControllerModule.controller('ResetPwdTwoCtrl', function($scope,$rootScope, $ionicPopup, $interval,$ionicPopup,$timeout, $location, $http, $state){
+
+});
