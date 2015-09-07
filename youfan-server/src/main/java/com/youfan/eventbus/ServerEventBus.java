ce9a781e41cbf4e8cd83a4df629e78c841946e9c
zhanghr <429763991@qq.com>
2015-09-07 09:40:00 +0800

    【运营端】update解决冲突

--- youfan-server/src/main/java/com/youfan/eventbus/ServerEventBus.java
+++ youfan-server/src/main/java/com/youfan/eventbus/ServerEventBus.java
@@ -1,32 +1,50 @@
 package com.youfan.eventbus;
 
+import java.util.Set;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.google.common.eventbus.AsyncEventBus;
+import com.google.common.eventbus.EventBus;
+
 /**
  * 
- * @description guava 事件处理
- * @author ZhangHuaRong   
+ * @description guava 事件处理采用同步的方式
+ * @author ZhangHuaRong
  */
 @Component("eventBus")
-public class ServerEventBus  {
+public class ServerEventBus implements InitializingBean, DisposableBean {
 
-	private AsyncEventBus asyncEventBus;
-	
+	private EventBus eventBus;
+	@Autowired
+	private Set<EventBusListener> listeners;
 
 	public ServerEventBus() {
-		asyncEventBus = EventBusFactory.getEventBusInstanll();
-		register(new ServerEventListener());
+		eventBus = EventBusFactory.getEventBus();
 	}
 
-	 public void register(Object object) {
-		 asyncEventBus.register(object);
-	 }
-     
-	 public void post(Object event) {
-		 asyncEventBus.post(event);
-	 }
+	public void register(Object object) {
+		eventBus.register(object);
+	}
+
+	public void post(Object event) {
+		eventBus.post(event);
+	}
+
+	@Override
+	public void destroy() throws Exception {
+
+	}
 	
 	
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		for (EventBusListener listerer : listeners) {
+			eventBus.register(listerer);
+		}
+	}
 
 }
