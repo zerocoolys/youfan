3be5194611c4e1dc2dd3f9b89a2c4cb1cc3ec04b
subdong <shuzichao@126.com>
2015-09-07 18:37:57 +0800

    [youfan-client] 添加地图拖动查询周边

--- youfan-client/www/lib/date/dateFormat.js
+++ youfan-client/www/lib/date/dateFormat.js
@@ -24,3 +24,4 @@ Date.prototype.Format = function (fmt) {
             fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
     return fmt;
 };
+
