3787711d8ad55ebbfbeb574f20862d07028c30b6
dolphineor <dolphineor@gmail.com>
2015-09-08 20:02:28 +0800

    [filter] Fix Request header field Authorization is not allowed

--- youfan-server/src/main/java/com/youfan/system/config/RestCORSFilter.java
+++ youfan-server/src/main/java/com/youfan/system/config/RestCORSFilter.java
@@ -26,7 +26,7 @@ public class RestCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept, Authorization");
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
