9c6b48981474deff1ad6e928b1348a72f7461f39
zhanghr <429763991@qq.com>
2015-09-17 11:17:43 +0800

    【运营端】add添加解析apk包获取版本信息

--- youfan-server/src/main/java/com/youfan/system/config/RestCORSFilter.java
+++ youfan-server/src/main/java/com/youfan/system/config/RestCORSFilter.java
@@ -27,7 +27,7 @@ public class RestCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept, Authorization");
-        filterChain.doFilter(servletRequest, servletResponse);
+       /* filterChain.doFilter(servletRequest, servletResponse);*/
     }
 
     @Override
